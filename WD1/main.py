d0973eaef91de3bbc91ae6a6c8534aab12863d42
sprajto <tomaszewski.lukasz@protonmail.com>
2024-03-03 10:36:43 +0100

    pierwszy commit

---
+++ WD1/main.py
@@ -0,0 +1,28 @@
+# jaki skrót do komentowania i odkomentowywania wielu linni
+# jak wyłączyć alerty o literówkach
+# co było mówione o nazwie zmiennych i podłodze? czy jest powód dla którego nie używamy camelCase'a
+# jak dostosować paterny / estetyke kodu , jak usuwać/dodawać spacje
+# jak wyrzucić konieczność dodawania komentarz do commitów
+a = 56
+print(a)
+print(type(a))
+b = 5.5
+print(b)
+print(type(b))
+zmienna_tekstowa = "wizualizacja danych"
+print(zmienna_tekstowa)
+print(type(zmienna_tekstowa))
+
+b = 2
+g = b**2
+print(g)
+h = pow(b,2)
+print(h)
+
+i = 6 ** (1/2)
+j = pow(6, 1/2)
+print(i)
+print(j)
+
+
+print("liczba a jest rowna {:d}, liczba b jest rowna {:d}".format(a,b))
